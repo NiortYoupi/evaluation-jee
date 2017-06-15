412ba2eb4928ab2006ed3f95b91fa663a92d4181
Unknown <laurent.esteve@soprasteria.com>
2017-06-15 16:52:17 +0200

    Implementation des types enum Branche et Niveau

--- src/com/sopra/model/constantes/Branche.java
+++ src/com/sopra/model/constantes/Branche.java
@@ -1,5 +1,19 @@
 package com.sopra.model.constantes;
 
 public enum Branche {
-
+	Commercial("Commercial"),
+	Ingenieur_Affaire("Ingénieur d'affaire"),
+	Manager("Manager");
+	
+	private String name="";
+	
+	private Branche(String name) {
+		this.name=name;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
 }
--- src/com/sopra/model/constantes/Niveau.java
+++ src/com/sopra/model/constantes/Niveau.java
@@ -1,5 +1,19 @@
 package com.sopra.model.constantes;
 
 public enum Niveau {
-
+	NiveauBac("Bac"),
+	NiveauBacPlus1("Bac +1"),
+	NiveauBacPlus2("Bac +2");
+	
+	private String name="";
+	
+	private Niveau(String name) {
+		this.name=name;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
 }
