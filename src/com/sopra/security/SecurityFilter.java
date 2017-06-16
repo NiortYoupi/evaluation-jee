7f1a7c50ad24e1c300e2817dd53ce6f4f35ef881
Unknown <laurent.esteve@soprasteria.com>
2017-06-16 11:36:05 +0200

    Ajout filtre

---
+++ src/com/sopra/security/SecurityFilter.java
@@ -0,0 +1,87 @@
+package com.sopra.security;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class SecurityFilter
+ */
+@WebFilter("/*")
+public class SecurityFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public SecurityFilter() {
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		System.out.println("DESTRUCTION DU FILTRE");
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest)req;
+		HttpServletResponse response = (HttpServletResponse)resp;
+		String myURI = request.getRequestURI();
+		
+		Set<SecurityMatch> myAcceptedURIs = new HashSet<>();
+		boolean needSecurityCheck = true;
+		
+		//Tout le monde
+		myAcceptedURIs.add(new SecurityMatch("js/", SecurityType.Anonyme));
+		myAcceptedURIs.add(new SecurityMatch("css/", SecurityType.Anonyme));
+		myAcceptedURIs.add(new SecurityMatch("font/", SecurityType.Anonyme));
+		myAcceptedURIs.add(new SecurityMatch("fonts/", SecurityType.Anonyme));
+		myAcceptedURIs.add(new SecurityMatch("/api", SecurityType.Anonyme));
+		myAcceptedURIs.add(new SecurityMatch("/erreur", SecurityType.Anonyme));
+		myAcceptedURIs.add(new SecurityMatch("/account/login", SecurityType.Anonyme));
+		
+		//Seulement les administrateurs
+		myAcceptedURIs.add(new SecurityMatch("/home", SecurityType.Administrateur));
+		
+		for (SecurityMatch forAcceptedURI : myAcceptedURIs)
+		{
+			if (forAcceptedURI.isLogged(myURI, (SecurityPersonne)request.getSession().getAttribute("utilisateur")))
+			{
+				needSecurityCheck = false;
+				break;
+			}
+		}
+		
+		
+		if (needSecurityCheck)
+		{
+			response.sendRedirect(String.format("%s/erreur", request.getContextPath()));
+			return;
+		}
+		
+		
+		chain.doFilter(request, response);
+
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		System.out.println("INITIALISATION DU FILTRE");
+	}
+
+}
